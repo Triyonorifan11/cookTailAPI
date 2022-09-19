d787a2dcd414a4b865bd1d595aaef1855b6744f8
Triyono Rifan <triyonorifan01@gmail.com>
2022-09-19 11:21:20 +0700

    star projek submition web development fundamental

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import main from './script/main.js'
+
+document.addEventListener('DOMContentLoaded', main)
